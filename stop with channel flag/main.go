14787c8022c265f5c190a2907c12a393fc400e69
Александр Козлов <aleksandrkozlov@big.local>
2025-07-14 13:56:30 +0300

    add task with: stop with channel flag

--- stop with channel flag/main.go
+++ stop with channel flag/main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	done := make(chan struct{})
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-done:
+				fmt.Println("Горутина завершила свою работу")
+				return
+			default:
+				fmt.Println("Что-то делаем")
+				time.Sleep(200 * time.Millisecond)
+			}
+		}
+	}()
+	fmt.Println("Ждем 10 итераций и закрваем канал")
+	for i := 0; i < 10; i++ {
+		fmt.Printf("Итерация %d\n", i)
+		time.Sleep(300 * time.Millisecond)
+	}
+	close(done)
+	wg.Wait()
+}
