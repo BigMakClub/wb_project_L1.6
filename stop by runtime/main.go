dd3ba71db8be7e8bb4839a9c205c622f0bb821fd
Александр Козлов <aleksandrkozlov@big.local>
2025-07-14 15:13:59 +0300

    done task using runtime.Goexit()

--- stop by runtime/main.go
+++ stop by runtime/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("Hello wildberries, i want to work here")
+		time.Sleep(1 * time.Second)
+		runtime.Goexit()
+		fmt.Println("I love movie Clerks by Kevin Smith, but nobody will know it, becouse of runtime.Goexit()")
+	}()
+
+	wg.Wait()
+}
